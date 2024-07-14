5e38f1acf99c9ea39c7b5319ae29d9fd3a9aa580
Louis-Marie Baer <lm@baermail.fr>
2024-07-14 14:31:56 +0200

    feat: pass error, remove anyhow

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use anyhow::Result;
-use std::{path::Path, process::Command, str::from_utf8};
+use std::{error::Error, path::Path, process::Command, str::from_utf8};
 
-pub fn get_password(path: &Path) -> Result<String> {
+pub fn get_password(path: &Path) -> Result<String, Box<dyn Error>> {
     let output = Command::new("pass").arg("show").arg(path).output()?;
     Ok(from_utf8(&output.stdout)?
         .trim_end_matches('\n')
