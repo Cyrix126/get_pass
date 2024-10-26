df50852e21be96e44b9579fa90f0132c574135dd
Louis-Marie Baer <lm@baermail.fr>
2024-10-26 14:19:01 +0200

    feat: add feature combine_with_url

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,15 @@ pub fn get_password(path: &Path) -> Result<String, Box<dyn Error>> {
         .trim_end_matches('\n')
         .to_string())
 }
+
+#[cfg(feature = "combine_with_url")]
+pub mod url {
+
+    use crate::{Error, Path};
+    use url::Url;
+    pub fn add_pass_to_url(url: &mut Url, path_pass: &Path) -> Result<(), Box<dyn Error>> {
+        Ok(url
+            .set_password(crate::get_password(path_pass).ok().as_deref())
+            .map_err(|_| std::fmt::Error)?)
+    }
+}
